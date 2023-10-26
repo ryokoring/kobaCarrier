48d04883e41f0712530ec2b20a2ca9983ca4f4ef
ryokoring <ryojikitamaru@gmail.com>
2023-10-26 15:07:44 +0900

    add header

--- src/pages/user/keeplist/index.jsx
+++ src/pages/user/keeplist/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Header from "../../../components/header"
 const KeepList = () => {
   return (
-    <div>KeepList</div>
+    <>
+    <Header></Header>
+    </>
   )
 }
 
