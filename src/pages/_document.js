11cf6509d1dc689161e44e5c8da1539f4fe0044c
ryokoring <ryojikitamaru@gmail.com>
2023-11-16 01:25:35 +0900

    add fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,10 +7,12 @@ export default function Document() {
       <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet"/>
       </Head>
       <body>
-      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet"></link>
+      {/* <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet"></link> */}
       <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet"></link>
       <link href="https://fonts.googleapis.com/earlyaccess/hannari.css" rel="stylesheet"></link>
-
+      {/* <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&family=Zen+Maru+Gothic&display=swap" rel="stylesheet"></link> */}
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&family=Zen+Maru+Gothic:wght@400;700&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;900&family=Zen+Maru+Gothic:wght@400;700&display=swap" rel="stylesheet"></link>
       
         <Main />
         <NextScript />
