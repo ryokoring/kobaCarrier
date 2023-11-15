57e728bd6599427844ff7d0b7bb98586f2990938
ryokoring <ryojikitamaru@gmail.com>
2023-11-16 01:25:04 +0900

    add pagination

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,58 @@
-import React from 'react'
-import Board from "../components/Board"
-import Header from "../components/header"
-import Tags from "../components/Tag"
-
-
+import React, { useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { useIntern } from "../../hooks/useIntern";
+import { useAllInterns } from "../../hooks/useAllInterns";
+import Board from "../components/Board";
+import Header from "../components/header";
+import Tags from "../components/Tag";
 export default function Home() {
-  useEffect
-const data = [
-  { 平均年収:"830万",開催時期: '12/24', 選考: "あり", 応募締切: '10/18' }
-]
-return (
-  <>
-  <Header></Header>
-  <Tags></Tags>
-  <Board data = {data}></Board>
-  <Board data = {data}></Board>
-  <Board data = {data}></Board>
-  <Board data = {data}></Board>
-  <Board data = {data}></Board>
-  <Board data = {data}></Board>
-  <Board data = {data}></Board>
-  <Board data = {data}></Board>
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 7;
 
-  </>
-)
+  // const {intern, isLoading, isError} = useIntern(3);
+  const { intern, isLoading, isError } = useAllInterns();
+  console.log("response", intern);
+  if (isError) return <div>Load is Failed</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  // データの例です。実際のデータ構造に合わせてください。
+
+  // 現在のページに表示するデータを計算
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = intern.slice(indexOfFirstItem, indexOfLastItem);
+
+  // ページ切り替え関数
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+  return (
+    <>
+      <Header></Header>
+      <Tags></Tags>
+      <Stack spacing={2} justifyContent="center" alignItems="center" style={{ margin: 20 }}>
+        <Pagination
+          count={Math.ceil(intern.length / itemsPerPage)}
+          variant="outlined"
+          color="secondary"
+          page={currentPage}
+          onChange={handlePageChange}
+        />
+      </Stack>
+      {currentItems.map((item, index) => (
+        <Board key={index} data={item} />
+      ))}
+      <Stack spacing={2} justifyContent="center" alignItems="center" style={{ margin: 20 }}>
+        <Pagination
+          count={Math.ceil(intern.length / itemsPerPage)}
+          variant="outlined"
+          color="secondary"
+          page={currentPage}
+          onChange={handlePageChange}
+        />
+      </Stack>
+    </>
+  );
 }
-
